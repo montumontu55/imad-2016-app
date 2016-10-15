989c61d44443c7d6b41c1396de90eab5991213c2
montumontu55 <montumontu55@gmail.com>
2016-10-15 17:09:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get(`/article-one`, function(req, res){
     path.join(__dirname, 'ui', 'article-one.html')
 });
 app.get(`/article-two`, function(req, res){
-    res.send('Article two requested and will be served here');
+    res.sendfile('Article two requested and will be served here');
 });
 app.get(`/article-three`, function(req, res){
     res.send('Article three requested and will be served here');
